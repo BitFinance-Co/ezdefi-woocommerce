e229867e3f35fd08c4c04a60121507b7fd086d40
Tien Nguyen <mrtienhp97@gmail.com>
2019-12-05 18:31:29 +0700

    fix procedure, remove event when uninstall

--- uninstall.php
+++ uninstall.php
@@ -17,3 +17,4 @@ $table_name = $wpdb->prefix . 'woocommerce_ezdefi_exception';
 $wpdb->query( "DROP TABLE IF EXISTS $table_name" );
 $wpdb->query( "DROP PROCEDURE IF EXISTS `wc_ezdefi_generate_amount_id`" );
 $wpdb->query( "DROP EVENT IF EXISTS `wc_ezdefi_clear_amount_table`" );
+$wpdb->query( "DROP EVENT IF EXISTS `wc_ezdefi_clear_exception_table`" );
