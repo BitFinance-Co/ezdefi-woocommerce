31d7be2db385f13d16924af2029f3f4b5c5a3e11
Tien Nguyen <mrtienhp97@gmail.com>
2019-12-16 07:02:59 +0700

    change procedure, lifetime plus 3605 seconds

--- woocommerce-gateway-ezdefi.php
+++ woocommerce-gateway-ezdefi.php
@@ -80,7 +80,6 @@ class WC_Ezdefi {
 	            IN value DECIMAl(20,12),
 			    IN token VARCHAR(10),
 			    IN decimal_number INT(2),
-			    IN life_time INT(11),
 			    OUT amount_id DECIMAL(20,12)
 			)
 			BEGIN
@@ -100,8 +99,8 @@ class WC_Ezdefi {
 			        SET amount_id = value;
 			    END IF;
 			    INSERT INTO $table_name (amount_key, price, amount_id, currency, expired_time) 
-			        VALUES (unique_id, value, amount_id, token, NOW() + INTERVAL life_time SECOND)
-                    ON DUPLICATE KEY UPDATE `expired_time` = NOW() + INTERVAL life_time SECOND;
+			        VALUES (unique_id, value, amount_id, token, NOW() + INTERVAL 3605 SECOND)
+                    ON DUPLICATE KEY UPDATE `expired_time` = NOW() + INTERVAL 3605 SECOND;
 			END
 		" );
 
