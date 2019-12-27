225430445a8e4708de9dd3f94d7f8190c1695792
Tien Nguyen <mrtienhp97@gmail.com>
2019-12-27 10:59:47 +0700

    sanitize $_POST value of ajax callback handle functions

--- includes/class-wc-ezdefi-ajax.php
+++ includes/class-wc-ezdefi-ajax.php
@@ -57,8 +57,12 @@ class WC_Ezdefi_Ajax
      */
     public function wc_ezdefi_get_currency_ajax_callback()
     {
-    	$keyword = $_POST['keyword'];
-	    $api_url = $_POST['api_url'];
+        if( ! isset( $_POST['keyword'] ) || ! isset( $_POST['api_url'] ) ) {
+	        wp_send_json_error( __( 'Can not get currency', 'woocommerce-gateway-ezdefi' ) );
+        }
+
+    	$keyword = sanitize_text_field( $_POST['keyword'] );
+	    $api_url = sanitize_text_field( $_POST['api_url'] );
 
 	    $api = new WC_Ezdefi_Api( $api_url );
 
@@ -124,13 +128,13 @@ class WC_Ezdefi_Ajax
 			wp_send_json_error( $message );
 		}
 
-		$uoid = $_POST['uoid'];
+		$uoid = sanitize_key( $_POST['uoid'] );
 
 		$data = array();
 
 		$data['order'] = $this->get_order( $uoid, __( 'Can not create payment', 'woocommerce-gateway-ezdefi' ) );
 
-		$data['method'] = $this->validate_payment_method( $_POST['method'], __( 'Can not create payment', 'woocommerce-gateway-ezdefi' ) );
+		$data['method'] = $this->validate_payment_method( sanitize_text_field( $_POST['method'] ), __( 'Can not create payment', 'woocommerce-gateway-ezdefi' ) );
 
 		return $data;
 	}
@@ -342,8 +346,14 @@ class WC_Ezdefi_Ajax
      */
     public function wc_ezdefi_check_order_status_ajax_callback()
     {
-    	$order_id = $_POST['order_id'];
+    	if( ! isset( $_POST['order_id'] ) ) {
+    		wp_die();
+	    }
+
+    	$order_id = sanitize_key( $_POST['order_id'] );
+
     	$order = wc_get_order( $order_id );
+
     	$status = $order->get_status();
 
     	wp_die( $status );
@@ -356,10 +366,13 @@ class WC_Ezdefi_Ajax
         $per_page = 15;
 
         if( isset( $_POST['page'] ) && $_POST['page'] > 1 ) {
-            $offset = $per_page * ( $_POST['page'] - 1 );
+        	$page = sanitize_key( $_POST['page'] );
+            $offset = $per_page * ( $page - 1 );
         }
 
-        $data = $this->db->get_exception( $_POST, $offset, $per_page );
+        $post_data = array_map( 'sanitize_text_field', $_POST );
+
+        $data = $this->db->get_exception( $post_data, $offset, $per_page );
 
         $total = $data['total'];
 
@@ -368,7 +381,7 @@ class WC_Ezdefi_Ajax
         $response = array(
             'data' => $data['data'],
             'meta_data' => array(
-                'current_page' => ( isset( $_POST['page'] ) ) ? (int) $_POST['page'] : 1 ,
+                'current_page' => ( isset( $_POST['page'] ) ) ? (int) sanitize_key( $_POST['page'] ) : 1 ,
                 'total' => (int) $total,
                 'total_pages' => $total_pages,
                 'offset' => $offset
@@ -385,7 +398,7 @@ class WC_Ezdefi_Ajax
         );
 
         if( isset( $_POST['keyword'] ) && ! empty( $_POST['keyword'] ) ) {
-	        $args['p'] = $_POST['keyword'];
+	        $args['p'] = sanitize_text_field( $_POST['keyword'] );
         }
 
 	    $orders = wc_get_orders( $args );
@@ -413,13 +426,13 @@ class WC_Ezdefi_Ajax
             wp_send_json_error();
         }
 
-        $amount_id = $_POST['amount_id'];
+        $amount_id = sanitize_key( $_POST['amount_id'] );
 
-        $currency = $_POST['currency'];
+        $currency = sanitize_key( $_POST['currency'] );
 
-	    $old_order_id = ( $_POST['old_order_id'] && ! empty( $_POST['old_order_id'] ) ) ? $_POST['old_order_id'] : null;
+	    $old_order_id = ( $_POST['old_order_id'] && ! empty( $_POST['old_order_id'] ) ) ? sanitize_key( $_POST['old_order_id'] ) : null;
 
-        $order_id = $_POST['order_id'];
+        $order_id = sanitize_key( $_POST['order_id'] );
 
 	    $order = wc_get_order( $order_id );
 
@@ -445,11 +458,11 @@ class WC_Ezdefi_Ajax
 			wp_send_json_error();
 		}
 
-		$amount_id = $_POST['amount_id'];
+		$amount_id = sanitize_key( $_POST['amount_id'] );
 
-		$currency = $_POST['currency'];
+		$currency = sanitize_key( $_POST['currency'] );
 
-		$order_id = $_POST['order_id'];
+		$order_id = sanitize_key( $_POST['order_id'] );
 
 		$order = wc_get_order( $order_id );
 
@@ -479,9 +492,9 @@ class WC_Ezdefi_Ajax
 
     public function wc_ezdefi_delete_amount_id_ajax_callback()
     {
-	    $amount_id = $_POST['amount_id'];
+	    $amount_id = sanitize_key( $_POST['amount_id'] );
 
-	    $order_id = ( ! empty( $_POST['order_id'] ) ) ? $_POST['order_id'] : null;
+	    $order_id = ( ! empty( $_POST['order_id'] ) ) ? sanitize_key( $_POST['order_id'] ) : null;
 
 	    $currency = $_POST['currency'];
 
