733c37eedbb6b7d6eed4c8acee6e3cf0e374a1e0
Tien Nguyen <mrtienhp97@gmail.com>
2020-02-25 14:31:53 +0700

    remove query when uninstall

--- uninstall.php
+++ uninstall.php
@@ -11,10 +11,6 @@ if ( defined( 'WC_REMOVE_ALL_DATA' ) && true === WC_REMOVE_ALL_DATA ) {
 }
 
 global $wpdb;
-$table_name = $wpdb->prefix . 'woocommerce_ezdefi_amount';
-$wpdb->query( "DROP TABLE IF EXISTS $table_name" );
 $table_name = $wpdb->prefix . 'woocommerce_ezdefi_exception';
 $wpdb->query( "DROP TABLE IF EXISTS $table_name" );
-$wpdb->query( "DROP PROCEDURE IF EXISTS `wc_ezdefi_generate_amount_id`" );
-$wpdb->query( "DROP EVENT IF EXISTS `wc_ezdefi_clear_amount_table`" );
 $wpdb->query( "DROP EVENT IF EXISTS `wc_ezdefi_clear_exception_table`" );
