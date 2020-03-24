17b7791804503d5da6eade379774d15b94ea1189
= <=>
2020-03-24 09:58:41 +0700

    fix uninstall function

--- uninstall.php
+++ uninstall.php
@@ -6,11 +6,10 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
 
-if ( defined( 'WC_REMOVE_ALL_DATA' ) && true === WC_REMOVE_ALL_DATA ) {
-	delete_option( 'woocommerce_ezdefi_settings' );
-}
-
 global $wpdb;
+
 $table_name = $wpdb->prefix . 'woocommerce_ezdefi_exception';
 $wpdb->query( "DROP TABLE IF EXISTS $table_name" );
 $wpdb->query( "DROP EVENT IF EXISTS `wc_ezdefi_clear_exception_table`" );
+
+delete_option( 'woocommerce_ezdefi_settings' );
